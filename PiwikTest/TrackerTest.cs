1c85fb4962c6deaa6a5f8c4a1046174900c6bdbe
Fabian Becker <halfdan@xnorfz.de>
2013-02-19 14:28:37 +0100

    Added Test project

---
+++ PiwikTest/TrackerTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PiwikTest
+{
+    [TestClass]
+    public class TrackerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
